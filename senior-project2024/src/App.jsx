b6599b470a9e1efdae384cfece4feb933285349a
elviazamora338 <elviazamora338@gmail.com>
2024-10-22 23:45:36 -0500

    signup react

--- senior-project2024/src/App.jsx
+++ senior-project2024/src/App.jsx
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
+import Add_Screen from './Components/Add_Page/Add_Screen';
+import All_Screen from './Components/Add_Page/Add_Screen';
+import Calendar_Screen from './Components/Calendar_Page/Calendar_Screen';
+import Home_Screen from './Components/Home_Page/Home_Screen';
+import Login_Screen from './Components/Login_Page/Login_Screen';
+import LoginAuth_Screen from './Components/LoginAuth_Page/LoginAuth_Screen';
+import SignUp_Screen from './Components/SignUp_Page/SignUp_Screen';
+
+
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='app'>
+               <Routes>
+               <Route path="/signup" element={<SignUp_Screen/>} />
+               <Route path="/home" element={<Home_Screen />} />
+                <Route path="/login" element={<Login_Screen />} />
+                {/* <Route path="/logout" element={<LogOut />} /> */}
+                {/* <Route path="/forgot" element={<ForgotPasswordPage />} /> */}
+              
+              
+                <Route path="/all" element={<All_Screen />} />
+                <Route path="/calendar" element={<Calendar_Screen />} />
+                <Route path="/loginauth" element={<LoginAuth_Screen />} />
+                {/* <Route path='/add' element={<Add_Screen />} /> */}
+                {/* <Route path='/account' element={<Account />} /> */}
+                {/* <Route path="*" element={<h1>Not Found</h1>} /> */}
+              </Routes>
+              {/* <div className='footerSpace'></div> */}
+            {/* <Footer  className="footer"/> */}
+          </div>
+        </Router>
   );
 }
 
---
+++ senior-project2024/src/Components/SignUp_Page/SignUp_Screen.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import './SignUp_Page.css'; // Assuming you have a CSS file for custom styling
+
+const SignUp_Screen = () => {
+  return (
+    <div className="container-fluid h-100">
+      <div className="row h-100">
+        {/* Left side of the screen for login */}
+        <div className="col-md-6 login-section">
+          <div className="login-box">
+            <h1 className="text-center">Get Started Now!</h1>
+            <br />
+            <div className="form-group login-button">
+              <label htmlFor="email">Email address (.edu only)</label>
+              <input type="email" className="form-control" id="email" placeholder="Enter your email" />
+            </div>
+            <br /><br />
+            <div className="form-group login-button">
+              <label htmlFor="university">Choose University</label>
+              <select className="form-select">
+                <option value="option0">None</option>
+                <option value="option1">University of Texas Rio Grande Valley - Edinburg</option>
+                <option value="option2">University of Texas Rio Grande Valley - Brownsville</option>
+              </select>
+            </div>
+            <br /><br />
+            <button type="submit" className="btn btn-primary w-100 login-button">Sign Up</button>
+            <div className="row p-2">
+              <div className="text-center">
+                <p>Have an account? <a href="#" className="nav-link d-inline p-0">Sign In</a></p>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* Right side of login screen */}
+        <div className="col-md-6 welcome-image"></div>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp_Screen;
