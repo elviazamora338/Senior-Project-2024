a1c929bb91ea748aa372f0bb84183d1e3b5117e1
Ana Garcia <112576637+anamichell@users.noreply.github.com>
2024-11-03 15:03:56 -0600

    Added inventory page in 'My Equipment'

--- senior-project2024/src/Components/MyEquip_Page/Inventory_Page.jsx
+++ senior-project2024/src/Components/MyEquip_Page/Inventory_Page.jsx
@@ -1,12 +1,12 @@
 // Import React and necessary hooks
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Requests_Page.css'
+import './Inventory_Page.css'
 import banner from '/Users/anagarcia/Desktop/Projects/Senior-Project-2024/senior-project2024/src/static/uni_banner/utrgv_banner.jpg'; // Import image from source
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap-icons/font/bootstrap-icons.css";
 
-const RequestsPage = () => {
+const InventoryPage = () => {
     return (
         <div className="d-flex">
             {/* Sidebar */}
@@ -90,69 +90,61 @@ const RequestsPage = () => {
                 <div className="col text-center m-3">
                     <div className="btn-group">
                         <Link to="/requests">
-                            <button type="button" className="bi bi-clock btn btn-secondary text-dark btn buttons-right"> Requests</button>
+                            <button type="button" className="bi bi-clock btn-outline-dark text-dark btn buttons-right"> Requests</button>
                         </Link>
                         <Link to="/inventory">
-                            <button type="button" className="bi bi-clipboard-fill btn btn-outline-dark text-dark border-secondary buttons-left"> Inventory</button>
+                            <button type="button" className="bi bi-clipboard-fill btn btn-secondary btn text-dark border-secondary buttons-left"> Inventory</button>
                         </Link>
                     </div>
                 </div>
 
-                {/* Table begins here */}
+                {/* Table and Cancel Button */}
                 <div className="container mt-4">
-                    <div className="row" >
+                    <div className="row">
                         <div className="col">
-                            <div className="requests">
+                            <div className="table-height">
                                 <table className="table">
-                                    <thead>
+                                    <thead classname="thead-bg">
                                         <tr>
-                                            <th>Name</th>
-                                            <th>Email</th>
+                                            <th>Image</th>
                                             <th>Device/Equipment</th>
-                                            <th>Status</th>
+                                            <th>Description</th>
+                                            <th>Number Available</th>
                                             <th>Action</th>
-                                            <th></th>
                                         </tr>
                                     </thead>
                                     <tbody>
                                         <tr>
-                                            <td>Jane Doe</td>
-                                            <td>jane.doe@utrgv.edu</td>
-                                            <td>Equipment Name</td>
-                                            <td>Approved</td>
                                             <td>
                                                 <div>
-                                                <button type="button" className="btn btn-secondary btn-sm">View</button>
+                                                    <i className="bi bi-image item-image me-2"></i>
                                                 </div>
                                             </td>
-                                        </tr>
-                                        <tr>
-                                            <td>John Doe</td>
-                                            <td>john.doe@utrgv.edu</td>
-                                            <td>Equipment Name</td>
-                                            <td>Pending Approval</td>
+                                            <td>ITEM 1</td>
                                             <td>
                                                 <div>
-                                                    <button type="button" className="bi bi-x btn btn-danger btn-sm me-2"></button>
-                                                    <button type="button" className="bi bi-check2 btn btn-success btn-sm me-2"></button>
-                                                    <button type="button" className="btn btn-secondary btn-sm">View</button>
+                                                    Model Info
+                                                    <span className="bi bi-dot"></span>
+                                                    <span className="bi bi-clock"></span>
+                                                    <span className="bi bi-dot"></span>
+                                                    Building
                                                 </div>
                                             </td>
+                                            <td>2</td>
+                                            <td className="checkbox-cell">
+                                                <button type="button" className="btn btn-secondary">Edit</button>
+                                            </td>
                                         </tr>
-                                        {/* end of table */}
-                                        
                                     </tbody>
                                 </table>
                             </div>
                         </div>
                     </div>
                 </div>
-
-                
             </div>
         </div>
     );
 };
 
-export default RequestsPage;
+export default InventoryPage;
 
