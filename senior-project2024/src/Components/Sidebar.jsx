8113034783f3d75676bdb2733e1914c6b7011a3d
Ana Garcia <112576637+anamichell@users.noreply.github.com>
2024-11-10 18:51:33 -0600

    Updated sidebar layout

--- senior-project2024/src/Components/Sidebar.jsx
+++ senior-project2024/src/Components/Sidebar.jsx
@@ -1,37 +1,36 @@
-// Sidebar.jsx
 import React from 'react';
-import { Link } from 'react-router-dom';
-import './Sidebar.css'
+import { NavLink } from 'react-router-dom'; // Use NavLink for active link management
+import './Sidebar.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap-icons/font/bootstrap-icons.css";
 
 const Sidebar = () => {
     return (
-        <div className="sidebar flex-shrink-0 p-3">
+        <div className="sidebar flex-shrink-0">
             <ul className="nav">
                 <li className="nav-item nav-icon">
-                    <Link to="/home" className="nav-link text-white">
+                    <NavLink to="/home" className="nav-link text-white" activeClassName="active">
                         <i className="bi bi-house-door"></i>
                         <span> Home</span>
-                    </Link>
+                    </NavLink>
                 </li>
                 <li className="nav-item nav-icon">
-                    <Link to="/all" className="nav-link text-white">
+                    <NavLink to="/all" className="nav-link text-white" activeClassName="active">
                         <i className="bi bi-grid"></i>
                         <span> All</span>
-                    </Link>
+                    </NavLink>
                 </li>
                 <li className="nav-item nav-icon">
-                    <Link to="/add" className="nav-link text-white">
+                    <NavLink to="/add" className="nav-link text-white" activeClassName="active">
                         <i className="bi bi-plus-circle"></i>
                         <span> Add</span>
-                    </Link>
+                    </NavLink>
                 </li>
                 <li className="nav-item nav-icon">
-                    <Link to="/requests" className="nav-link text-white">
-                        <i className="bi bi-box"></i>
+                    <NavLink to="/requests" className="nav-link text-white">
+                        <i className="bi bi-box" activeClassName="active"></i>
                         <span> My Equipment</span>
-                    </Link>
+                    </NavLink>
                 </li>
             </ul>
         </div>
